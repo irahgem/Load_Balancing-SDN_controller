754a1a9c49c5437f4deb18d5918b68b5496996a3
Harish <harishbharish6@gmail.com>
2022-12-29 19:30:34 +0530

    Commited

---
+++ data/heavy_load.py
@@ -0,0 +1,26 @@
+'''
+This file contains code where the load on switches is more.
+This means that the arrival rate on the switches will be much higher than the service rate.
+
+In this case we consider the following rate of values to generate the data - 
+λ = random numbers in range(50, 100)
+μ = a random number in range(1, 5)
+'''
+
+import pandas as pd
+import numpy as np
+
+
+def gen_lambda_mu_lists(switch_count):
+    # Randomly generating lambda and mu values (same for all) for 34 switches
+    arrival_lambda_list = list(np.random.randint(
+        low=50, high=100, size=switch_count))
+    service_mu_list = [np.random.randint(low=1, high=5)]*switch_count
+
+    return arrival_lambda_list, service_mu_list
+
+
+def gen_data_csv(table_data):
+    final_data = pd.DataFrame(table_data)
+    final_csv_data = final_data.to_csv('csv/data_heavy.csv')
+    return final_data
---
+++ main.py
@@ -0,0 +1,73 @@
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+from datetime import datetime
+from data import heavy_load, light_load, skewed_load, table
+from graph_building import build_graph
+from q_learning import q_learning
+from switch_migration import calculate_controller_cluster_load_ratio, calculate_discrete_coefficient
+from graph_helpers import calculate_Dij_from_states, show_all_plots
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# Initialise the number of controllers and the gml files to use.
+k = int(input("Enter the number of controllers to allocate: "))
+graph = nx.read_gml('gml/Arnes.gml', label='id')
+switch_count = len(graph.nodes)
+
+# ----------------------------------------------------------------------------------------------------------------
+# log_file_name = "logs/Log_message{}.log".format(datetime.now().time())
+# log_file = open(log_file_name, "w")
+# print("Check the system logs at - " + log_file_name + "\n")
+# sys.stdout = log_file
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# Create CSV files for different load scenarios
+# (i) Heavy Load
+print('------------------- Heavy Load -------------------')
+arrival_lambda_list, service_mu_list = heavy_load.gen_lambda_mu_lists(switch_count)
+table_data = table.generate_table(arrival_lambda_list, service_mu_list, switch_count)
+final_data_heavy = heavy_load.gen_data_csv(table_data)
+
+# (ii) Light Load
+print('------------------- Light Load -------------------')
+arrival_lambda_list, service_mu_list = light_load.gen_lambda_mu_lists(switch_count)
+table_data = table.generate_table(arrival_lambda_list, service_mu_list, switch_count)
+final_data_light = light_load.gen_data_csv(table_data)
+
+# (iii) Skewed Load
+print('------------------- Skewed Load -------------------')
+arrival_lambda_list, service_mu_list = skewed_load.gen_lambda_mu_lists(switch_count)
+table_data = table.generate_table(arrival_lambda_list, service_mu_list, switch_count)
+final_data_skewed = skewed_load.gen_data_csv(table_data)
+
+# ----------------------------------------------------------------------------------------------------------------
+final_data = final_data_heavy
+controllers_Q, controller_sets_Q, controller_sets_G, load_array = build_graph(k, graph, final_data)
+states_list, disc_reward_list, num_switch_exchanges_list = q_learning(
+    controller_sets_Q, controllers_Q, k, switch_count, load_array)
+d_coeff_list = calculate_Dij_from_states(k, states_list, controllers_Q, load_array)
+
+show_all_plots(d_coeff_list, disc_reward_list, num_switch_exchanges_list, 'Load Heavy')
+
+# ----------------------------------------------------------------------------------------------------------------
+final_data = final_data_light
+controllers_Q, controller_sets_Q, controller_sets_G, load_array = build_graph(k, graph, final_data)
+states_list, disc_reward_list, num_switch_exchanges_list = q_learning(
+    controller_sets_Q, controllers_Q, k, switch_count, load_array)
+d_coeff_list = calculate_Dij_from_states(k, states_list, controllers_Q, load_array)
+
+show_all_plots(d_coeff_list, disc_reward_list, num_switch_exchanges_list, 'Load Light')
+
+# ----------------------------------------------------------------------------------------------------------------
+final_data = final_data_skewed
+controllers_Q, controller_sets_Q, controller_sets_G, load_array = build_graph(k, graph, final_data)
+states_list, disc_reward_list, num_switch_exchanges_list = q_learning(
+    controller_sets_Q, controllers_Q, k, switch_count, load_array)
+d_coeff_list = calculate_Dij_from_states(k, states_list, controllers_Q, load_array)
+
+show_all_plots(d_coeff_list, disc_reward_list, num_switch_exchanges_list, 'Load Skewed')
+
+# ----------------------------------------------------------------------------------------------------------------
+# sys.stdout = sys.__stdout__
